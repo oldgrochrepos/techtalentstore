2824dc66aeafae4efea594d3369f863597f519da
Aaron Groch <ar.groch@gmail.com>
2014-05-28 12:23:05 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Techtalentstore::Application.config.secret_key_base = '102c71a3ba6d6897d58e8628d079c749c1c2d42e1263f03b3e3c4dafaf147bbd187ed1629104bb58d28febd5bf71c1a4317084b0190cc70d5b3b7e83bb6af9ed'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Techtalentstore::Application.config.session_store :cookie_store, key: '_techtalentstore_session'
